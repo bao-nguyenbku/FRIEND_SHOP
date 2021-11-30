6c2ec8110638ab45ba4d2fa216931634d1604bef
Bao Nguyen Thien <nguyenthienbao0402@gmail.com>
2021-11-30 09:44:18 +0700

    add product

--- db/db.php
+++ db/db.php
@@ -11,3 +11,15 @@ $db = new mysqli($servername, $username, $password, $dbname, $port);
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
+
+// $servername = "localhost";
+// $username = "root"; // default username for localhost is root
+// $password = ""; // default password for localhost is empty
+// $dbname = "friend_shop"; // database name
+
+// // Create connection
+// $db = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($db->connect_error) {
+//     die("Connection failed: " . $db->connect_error);
+// }
